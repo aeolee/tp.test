04a788a74e53364949d2d2f6b32f6f55842ece0b
aeolee <aeoluslee@qq.com>
2018-12-14 12:41:57 +0800

    完成编辑后数据没改会出错

--- application/index/controller/Klass.php
+++ application/index/controller/Klass.php
@@ -59,9 +59,10 @@ class Klass extends Index
         return $this->success('操作成功',url('/klass/index'));
     }
 
+    
     public function edit()
     {
-        $id = Request::instance()->parameter('id/d');
+        $id = Request::instance()->param('id/d');
 
         $teachers = Mt::all();
         $this->assign('teachers',$teachers);
@@ -73,5 +74,34 @@ class Klass extends Index
 
         $this->assign('Klass',$Klass);
         return $this->fetch();
+    }
+
+    /**
+     * 
+     * @route('/klass/update$')
+     */
+    public function update()
+    {
+        $id = Request::instance()->post('id/d');
+
+        $Klass = Ks::get($id);
+        if(is_null($Klass)){
+            return $this->error('系统未找到ID为'.$id.'的记录');
+        }
+
+        $Klass->name = Request::instance()->post('name');
+        $Klass->teacher_id = Request::instance()->post('teacher_id/d');
+
+        $validate = new \app\common\validate\Klass;
+        $result = 1;
+
+        if($validate->check($Klass->getData())){
+            $result = $Klass->save();
+        } 
+        if(!$result){
+            return $this->error('更新错误:'.$validate->getError());
+        }
+
+        return $this->success('操作成功',url('/klass/index'));
     }
 }
